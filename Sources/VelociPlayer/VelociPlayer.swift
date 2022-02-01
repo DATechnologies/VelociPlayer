388e43edf6993bb9aa2a4e5e1354983c6f4bd720
Ethan Humphrey <ehumphrey@rapptrlabs.edu>
2022-02-01 09:49:11 -0500

    Decrease periodic interval to run at 120fps

--- Sources/VelociPlayer/VelociPlayer.swift
+++ Sources/VelociPlayer/VelociPlayer.swift
@@ -136,7 +136,7 @@ public class VelociPlayer: AVPlayer {
     }
     
     func startObservingPlayer() {
-        timeObserver = self.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.05, preferredTimescale: 1000), queue: .main) { [weak self] time in
+        timeObserver = self.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.005, preferredTimescale: 10000), queue: .main) { [weak self] time in
             self?.onPlayerTimeChanged(time: time)
         }
         
