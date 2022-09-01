a28684bdcdd8f7c85cf688d4ed638c4ba4d8b87e
Ethan Humphrey <ehumphrey@rapptrlabs.edu>
2022-09-01 18:32:20 -0400

    Fix NaN crash

--- Sources/VelociPlayer/Source/VelociPlayer+Buffering.swift
+++ Sources/VelociPlayer/Source/VelociPlayer+Buffering.swift
@@ -23,6 +23,11 @@ extension VelociPlayer {
     
     internal func updateBufferTime(timeRanges: [NSValue]) {
         if let timeRange = timeRanges.first?.timeRangeValue {
+            guard !timeRange.end.seconds.isNaN && !duration.seconds.isNaN && !duration.seconds.isZero else {
+                self.bufferTime = CMTime(seconds: 0, preferredTimescale: 10_000)
+                self.bufferProgress = 0
+                return
+            }
             self.bufferTime = timeRange.end
             self.bufferProgress = timeRange.end.seconds / duration.seconds
         }
